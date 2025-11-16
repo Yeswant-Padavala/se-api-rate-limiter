a40b0075deb0916e89311f70f7f6cabb5ae405b7
Vishal Naik <vishalnaik7111@gmail.com>
2025-11-16 14:25:12 +0530

    Completed API rate limiter + all tests passing

--- src/models/policyModel.js
+++ src/models/policyModel.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 // Hardcoded default policies (can be stored in Redis or DB later)
 export const policies = [
   { id: 1, name: "default", limit: 100, window: "1m" },
   { id: 2, name: "premium", limit: 1000, window: "1m" }
 ];
-=======
-// Hardcoded default policies (can be stored in Redis or DB later)
-export const policies = [
-  { id: 1, name: "default", limit: 100, window: "1m" },
-  { id: 2, name: "premium", limit: 1000, window: "1m" }
-];
->>>>>>> 5786cad023d9787b6f4ab3dff286dd80d70a5df1
--- src/routes/policyRoutes.js
+++ src/routes/policyRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import { getPolicies, createPolicy } from "../controllers/policyController.js";
 import { validatePolicy } from "../middleware/validation.js";
@@ -12,18 +11,3 @@ router.get("/", getPolicies);
 router.post("/", validatePolicy, createPolicy);
 
 export default router;
-=======
-import express from "express";
-import { getPolicies, createPolicy } from "../controllers/policyController.js";
-import { validatePolicy } from "../middleware/validation.js";
-
-const router = express.Router();
-
-// 🟢 GET all policies
-router.get("/", getPolicies);
-
-// 🟣 POST new policy
-router.post("/", validatePolicy, createPolicy);
-
-export default router;
->>>>>>> 5786cad023d9787b6f4ab3dff286dd80d70a5df1
