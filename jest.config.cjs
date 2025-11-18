9ea4bf3966512e3b4a7569c7596d7c3570e62f44
Yeswant-Padavala <yeswant.padavala@gmail.com>
2025-11-18 08:55:58 +0530

    Modified jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,14 +2,10 @@ module.exports = {
   testEnvironment: "node",
   transform: {},
   extensionsToTreatAsEsm: [".js"],
-
   moduleFileExtensions: ["js", "json", "node"],
-
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.js"],
   coverageReporters: ["json", "lcov", "text", "clover"],
-
   coverageDirectory: "coverage",
-
-  verbose: true,
+  verbose: true
 };
