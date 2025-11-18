048c32e52e1b2281b146376b8bba955d926192d5
Yeswant-Padavala <yeswant.padavala@gmail.com>
2025-11-18 14:48:56 +0530

    Fixed tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,9 @@
-export default {
+module.exports = {
   testEnvironment: "node",
   transform: {},
-  verbose: true,
   roots: ["<rootDir>/tests"],
-  moduleFileExtensions: ["js", "json", "node"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.js"],
-  coverageDirectory: "coverage"
+  moduleFileExtensions: ["js", "json", "node"],
+  verbose: true
 };
